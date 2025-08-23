54171eb82481d6850ceb4f20fc24caec26dabe18
Fripouney <133747510+Fripouney@users.noreply.github.com>
2025-08-24 00:05:58 +0200

    [ISSUE #8] Better code quality (#9)
    * Added docstrings for methods and classes
    * Fixed a bug when saving file for the first time

--- config.py
+++ config.py
@@ -1,20 +1,32 @@
 import tkinter as tk
 
 class Config:
+    """
+    Configuration for different components of the editor
+    """
     @staticmethod
     def add_text_area_bindings(text_area):
+        """
+        Add key bindings for text formatting in the text area
+        """
         text_area.bind("<Control-b>", lambda event: Config.toggle_tag(text_area, "bold"))
         text_area.bind("<Control-i>", lambda event: Config.toggle_tag(text_area, "italic"))
         text_area.bind("<Control-u>", lambda event: Config.toggle_tag(text_area, "underline"))
 
     @staticmethod
     def config_tags(text_area):
+        """
+        Configure text area tags for the editor
+        """
         text_area.tag_configure("bold", font=("Arial", 12, "bold"))
         text_area.tag_configure("italic", font=("Arial", 12, "italic"))
         text_area.tag_configure("underline", font=("Arial", 12, "underline"))
 
     @staticmethod
     def toggle_tag(text_area, tag, event=None):
+        """
+        Toggle a text area tag on the selected text
+        """
         try:
             current_tags = text_area.tag_names("sel.first")
             if tag in current_tags:
@@ -23,12 +35,14 @@ class Config:
                 text_area.tag_add(tag, "sel.first", "sel.last")
         except tk.TclError:
             print("No text selected to toggle tag.")
-            pass
 
         return "break"
-    
+
     @staticmethod
     def set_status_bar(status_bar, text):
+        """
+        Set the text of the status bar
+        """
         status_bar.config(state="normal")
         status_bar.delete(1.0, tk.END)
         status_bar.insert(tk.END, text)
